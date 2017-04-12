c230b8e59c5bc176861c0d31b01f62c836e28daa
Milan Kloewer <kloewer@dawson.atm.ox.ac.uk>
2017-04-12 15:23:58 +0100

    nc attr added

--- swm_integration.py
+++ swm_integration.py
@@ -108,9 +108,7 @@ def feedback_ini(u,v,h,t):
         output_param()      # store the param dictionnary
         
         # Store information in txt file
-        # TODO output maybe all scalar values in param dictionary?
         output_txt('Integrating %.1f days with dt=%.2f min in %i time steps' % (param['Ndays'],dt/60.,param['Nt']))    
-        output_txt('and eddy viscosities A = %4.1f, B = %4.1f' % (param['nu_A'],param['nu_B']))
         output_txt('Time integration scheme is '+param['scheme']+' with CFL = %.2f' % param['cfl'])
         output_txt('')
         output_txt('Starting shallow water model on '+tictoc.asctime())
@@ -123,8 +121,8 @@ def feedback(u,v,h,t,tic):
         if param['output']:     # storing u,v,h as netCDF4
             output_nc(u,v,h,t)
     
-    # feedback on progress every integer % step.
-    if ((i+1)/param['Nt']*100 % 1) < (i/param['Nt']*100 % 1):
+    # feedback on progress every 5% step.
+    if ((i+1)/param['Nt']*100 % 5) < (i/param['Nt']*100 % 5):
         progress = str(int((i+1)/param['Nt']*100.))+'%'
         print(progress, end='\r')
         if i > 100:
